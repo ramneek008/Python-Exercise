fe03599248dbd31f37877c86b4e105c3b62f26e1
Ramneek Singh <48586823+ramneek008@users.noreply.github.com>
2020-04-06 18:28:44 +0530

    init

---
+++ main.py
@@ -0,0 +1,12 @@
+def getInteger():
+    result = int(input("Enter integer: "))
+    return result
+
+def Main():
+    print("Started")
+    output = getInteger()
+    print(output)
+
+if __name__ == "__main__":
+    Main()
+    
